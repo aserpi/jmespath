8cd7410e5f0c2e75737a18eab858e6901a760fbb
Alessandro Serpi <alessandro.serpi@me.com>
2024-12-14 17:52:48 +0100

    Add unroll function modes

--- bin/jmespath_splunk_functions.py
+++ bin/jmespath_splunk_functions.py
@@ -4,6 +4,7 @@ import sys
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "lib"))
 
+import jmespath.exceptions
 import jmespath.functions
 
 
@@ -23,13 +24,33 @@ class JmespathSplunkFunctions(jmespath.functions.Functions):
             return arg
 
     @jmespath.functions.signature({"types": ["array"]}, {"types": ["string"]},
-                                  {"types": ["string"]})
-    def _func_unroll(self, array, key_key, value_key):
+                                  {"types": ["string"]}, {"types": ["string"],
+                                                          "variadic": True})
+    def _func_unroll(self, array, key_key, value_key, *arguments):
         """Build an object from an array of objects with key value pairs.
 
-        Example: unroll([{"Key": "Pair key", "Value": "Pair value"}], "Key", "Value")
-        produces {"Pair key": "Pair value"}.
+        The last argument defines the behavior in case of multiple object with
+        the same key:
+        - all: Create an array with all the objects
+        - first: Discard the new value and keep existing objects
+        - last: Keep the new value overwriting existing objects
+
+        Example: unroll([{"Key": "Pair key", "Value": "Pair value A"},
+                         {"Key": "Pair key", "Value": "Pair value B"}],
+                        "Key", "Value", "all")
+        produces {"Pair key": ["Pair value A", "Pair value B"]}.
         """
+        if len(arguments) != 1:
+            raise jmespath.exceptions.JMESPathError(
+                f"syntax-error: unroll() expects at most a single 'mode' "
+                f"argument, but received {len(arguments)} instead.")
+
+        mode = arguments[0]
+        if mode != "all" and mode != "first" and mode != "last":
+            raise jmespath.exceptions.JMESPathError(
+                f"syntax-error: unroll() expects the mode to be 'all', "
+                f"'first', or 'last', but received `{mode}` instead.")
+
         object_ = {}
         for item in array:
             try:
@@ -38,17 +59,16 @@ class JmespathSplunkFunctions(jmespath.functions.Functions):
                 if not isinstance(key, str):
                     key = str(key)
 
-                # TODO: User option: Overwrite, keep, or make multivalue.
-                if key not in object_:
-                    object_[key] = value
-                elif isinstance(object_[key], list):
-                    object_[key].append(value)
+                if key in object_:
+                    if mode == "all":
+                        object_[key].append(value)  # Already a list
+                    elif mode == "last":
+                        object_[key] = value
                 else:
-                    # Opportunistically convert into an array to hold multiple values.
-                    # Generally harmful to structured data, but plays nice with Splunk's multivalue
-                    # fields.
-                    object_[key] = [object_[key], value]
+                    if mode == "all":
+                        object_[key] = [object_[key]]
+                    else:
+                        object_[key] = value
             except KeyError:
-                # If either field is missing, just silently move on
-                continue
+                continue  # If either field is missing, just silently move on
         return object_
